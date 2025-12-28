99076a4af32ffcfa83980056735585fe5991562d
waltersilva5 <waltersilva5@outlook.com>
2025-12-28 16:35:34 -0300

    feature: langgraph example

--- ia/langchain/1_first_project/2_agent_with_tools_example_calc/main.py
+++ ia/langchain/1_first_project/2_agent_with_tools_example_calc/main.py
@@ -1,28 +1,34 @@
-from langchain.agents import initialize_agent, AgentType
 from langchain_ollama import ChatOllama
 from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent
 
-# Usando o decorator @tool para registrar a função 'somar' como uma ferramenta
+# Usando o decorator @tool para registrar funções como ferramentas
 @tool
-def somar(a: int, b: int) -> int:
+def sum(a: int, b: int) -> int:
     """
     Soma dois números e retorna o resultado.
     """
     return a + b
 
+@tool
+def mult(a: int, b: int) -> int:
+    """
+    Multiplica dois números e retorna o resultado.
+    """
+    return a * b
+
 llm = ChatOllama(model="qwen2.5:3b", temperature=0)
 
-tools = [somar]
+tools = [sum, mult]
 
-agent = initialize_agent(
-    tools,
-    llm,
-    agent_type=AgentType.OPENAI_FUNCTIONS,
-    verbose=True
-)
+# Criando o agente com LangGraph
+agent = create_react_agent(llm, tools)
 
 # Perguntando ao agente
-response = agent.invoke({"input": "Qual é a soma de 2 e 3?"})
+print("calculating sum...")
+response_sum = agent.invoke({"messages": [{"role": "user", "content": "Qual é a soma de 2 e 3?"}]})
+print("Response sum:", response_sum["messages"][-1].content)
 
-# Exibindo a resposta
-print("Response:", response["output"])
+print("\ncalculating multiplication...")
+response_mult = agent.invoke({"messages": [{"role": "user", "content": "Qual é o produto de 4 e 5?"}]})
+print("\n\n\nResponse mult:", response_mult["messages"][-1].content)
